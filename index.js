7faa2fde44dc4cc8282b13b8152cc37a7767a226
Jim Fowler <fowler@math.osu.edu>
2017-11-06 09:08:39 -0500

    testing again

--- index.js
+++ index.js
@@ -64,7 +64,25 @@ function protocolAndHostname( req ) {
     return 'http://' + req.hostname + ':3000/';
 }
 
-/*
+app.get('/:code/:path(*)', function (req, res) {
+    if (secretCode(req.params.path) == req.params.code) {
+	var filename = path.join(__dirname, req.params.path);
+	fs.stat(filename, function(err, stats) {
+	    if (err) {
+		res.status(500).send(err);
+		return;
+	    }
+	    
+	    var file = fs.createReadStream(filename);
+	    res.setHeader('Content-Length', stats.size);
+	    res.setHeader('Content-Type', 'application/pdf');
+	    file.pipe(res);
+	});
+    } else {
+	res.status(403).send('You do not have the passphrase for that file.');
+    }
+});
+
 app.get('/:path(*)/lti.xml', function(req, res) {
     var hash = {
 	title: 'Otterscan',
@@ -75,7 +93,6 @@ app.get('/:path(*)/lti.xml', function(req, res) {
         
     res.render('config', hash);
 });
-*/
 
 var pug = require('pug');
 var passback = pug.compileFile(path.join(__dirname,'views/passback.pug'));
@@ -201,25 +218,6 @@ app.post('/2/:path(*)/lti', function(req, res, next) {
       
   })(req, res, next);
 
-});
-
-app.get('/:code/:path(*)', function (req, res) {
-    if (secretCode(req.params.path) == req.params.code) {
-	var filename = path.join(__dirname, req.params.path);
-	fs.stat(filename, function(err, stats) {
-	    if (err) {
-		res.status(500).send(err);
-		return;
-	    }
-	    
-	    var file = fs.createReadStream(filename);
-	    res.setHeader('Content-Length', stats.size);
-	    res.setHeader('Content-Type', 'application/pdf');
-	    file.pipe(res);
-	});
-    } else {
-	res.status(403).send('You do not have the passphrase for that file.');
-    }
 });
 
 app.listen(config.port, function () {
